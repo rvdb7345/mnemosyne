b2c93872cd1936cc42d341a0e5652a2138e4fc94
rvdb7345 <rvdb7345@gmail.com>
2024-11-12 20:17:53 +0100

    update to main.py for secret reading

--- streamlit-app/src/main.py
+++ streamlit-app/src/main.py
@@ -4,9 +4,11 @@ from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 import os
 import pandas as pd
-from utils.helpers import create_dir  # Ensure this module exists and has necessary functions
+from utils.helpers import create_dir 
+from utils.file_paths import add_project_to_path, ProjectPaths
 
-
+pp = ProjectPaths()
+add_project_to_path(pp)
 
 # Initialize base directories
 EXERCISES_DIR = 'exercises'
@@ -14,56 +16,14 @@ PROGRESS_DIR = 'progress'
 create_dir(EXERCISES_DIR)
 create_dir(PROGRESS_DIR)
 
-# Load configuration
-# with open('config.yaml') as file:
-#     config = yaml.load(file, Loader=SafeLoader)
-
-
 st.set_page_config(page_title="Language Learning App", layout="wide")
 
-# Load the config
-def load_config():
-    with open('config.yaml') as file:
-        return yaml.load(file, Loader=SafeLoader)
-
-# Helper to determine if password is alredy hashed
-def is_bcrypt_hash(s):
-    return s.startswith(('$2a$', '$2b$', '$2x$', '$2y$')) and len(s) == 60
-
-
-# Hash new plaintext passwords only
-def hash_plaintext_passwords(config):
-    plaintext_passwords = {}
-    for user, details in config['credentials']['usernames'].items():
-        # Check if the password is not a bcrypt hash
-        if not is_bcrypt_hash(details['password']):
-            plaintext_passwords[user] = details['password']
-
-    if plaintext_passwords:
-        hashed_passwords = stauth.Hasher(list(plaintext_passwords.values())).generate()
-        for user, hashed_pw in zip(plaintext_passwords.keys(), hashed_passwords):
-            config['credentials']['usernames'][user]['password'] = hashed_pw
-
-    return config
-
-
-# Save the config
-def save_config(config):
-    with open('config.yaml', 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
-
-config = load_config()
-
-if 'hashed_done' not in st.session_state:
-    config = hash_plaintext_passwords(config)
-    save_config(config)
-    st.session_state.hashed_done = True
-
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
+    dict(st.secrets['credentials']),
+    st.secrets['cookie']['name'],
+    st.secrets['cookie']['key'],
+    st.secrets['cookie']['expiry_days'],
+    st.secrets['preauthorized']
 )
 
 def main():
