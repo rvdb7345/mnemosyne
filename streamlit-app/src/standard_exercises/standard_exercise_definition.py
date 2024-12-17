579956f00f8803265785e53657ef0917eb168b8a
rvdb7345 <rvdb7345@gmail.com>
2024-12-17 20:48:09 +0100

    added frequency list for turkish and split up the frquency list for spanish

--- streamlit-app/src/standard_exercises/standard_exercise_definition.py
+++ streamlit-app/src/standard_exercises/standard_exercise_definition.py
@@ -88,14 +88,113 @@ class SpanishVocabList(VocabList):
             target_language_name='English',
             exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified.txt"
         )
+        
+class SpanishVocabList1000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Spanish Frequency List 0 - 1000",
+            source_language_name='Spanish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified1000.txt"
+        )
+        
+class SpanishVocabList2000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Spanish Frequency List 1000 - 2000",
+            source_language_name='Spanish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified2000.txt"
+        )
 
-class TurkishVocabList(VocabList):
+class SpanishVocabList3000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Spanish Frequency List 2000 - 3000",
+            source_language_name='Spanish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified3000.txt"
+        )
+
+class SpanishVocabList4000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Spanish Frequency List 3000 - 4000",
+            source_language_name='Spanish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified4000.txt"
+        )
+
+class SpanishVocabList5000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Spanish Frequency List 4000 - 5000",
+            source_language_name='Spanish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/SpanishEnglishFrequencySimplified5000.txt"
+        )
+
+class TurkishDuolingoList(VocabList):
     def __init__(self):
         super().__init__(
             exercise_name="Turkish Duolingo List",
             source_language_name='Turkish',
             target_language_name='English',
             exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishDuolingo.txt"
+        )
+        
+class TurkishVocabList(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified.txt"
+        )
+
+class TurkishVocabList1000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List 0 - 1000",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified1000.txt"
+        )
+
+class TurkishVocabList2000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List 1000 - 2000",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified2000.txt"
+        )
+
+class TurkishVocabList3000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List 2000 - 3000",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified3000.txt"
+        )
+
+class TurkishVocabList4000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List 3000 - 4000",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified4000.txt"
+        )
+        
+class TurkishVocabList5000(VocabList):
+    def __init__(self):
+        super().__init__(
+            exercise_name="Turkish Frequency List 4000 - 5000",
+            source_language_name='Turkish',
+            target_language_name='English',
+            exercise_path="streamlit-app/src/standard_exercises/TurkishEnglishFrequencySimplified5000.txt"
         )
 
 # Add more predefined VocabList subclasses here as needed
